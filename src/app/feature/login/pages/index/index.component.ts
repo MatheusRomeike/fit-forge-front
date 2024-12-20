2cbf14d8e362b42994b3651d8d84dd93b3879325
Matheus <matheus_romeike@sisand.com.br>
2024-12-20 20:09:01 -0300

    adding image roulette

--- src/app/feature/login/pages/index/index.component.ts
+++ src/app/feature/login/pages/index/index.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrl: './index.component.scss',
+  styleUrls: ['./index.component.scss'], // Corrigido para `styleUrls` no plural
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
   step: string = 'login';
+  index = 1;
+  currentOpacity = 1;
+
+  ngOnInit(): void {
+    this.drawIndex();
+    this.startImageRotation();
+  }
 
   changeStep(step: string) {
     this.step = step;
-    console.log(this.step);
+  }
+
+  drawIndex() {
+    const imagesIndex = [1, 2, 3];
+    let newIndex;
+
+    do {
+      newIndex = imagesIndex[Math.floor(Math.random() * imagesIndex.length)];
+    } while (newIndex === this.index);
+
+    this.index = newIndex;
+  }
+
+  startImageRotation() {
+    setInterval(() => {
+      this.fadeOut(() => {
+        this.drawIndex();
+        this.fadeIn();
+      });
+    }, 10000);
+  }
+
+  fadeOut(callback: () => void) {
+    this.currentOpacity = 0;
+    setTimeout(callback, 1000);
+  }
+
+  fadeIn() {
+    this.currentOpacity = 1;
   }
 }
